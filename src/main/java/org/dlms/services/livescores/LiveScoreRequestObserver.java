dd14ce057cf9ad3d9c44f0684750bd04c2c531fd
ankitkumar <ankit.kumar071460@gmail.com>
2024-11-01 22:55:55 +0530

    add backpressure handling

--- src/main/java/org/dlms/services/livescores/LiveScoreRequestObserver.java
+++ src/main/java/org/dlms/services/livescores/LiveScoreRequestObserver.java
@@ -1,23 +1,39 @@
 package org.dlms.services.livescores;
 
+import io.grpc.stub.ServerCallStreamObserver;
 import io.grpc.stub.StreamObserver;
 import org.dlms.services.ScoreRequest;
 import org.dlms.services.ScoreResponse;
+
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import static org.dlms.utils.DlmsLogger.logInfo;
 
 public class LiveScoreRequestObserver {
 
-    private final StreamObserver<ScoreResponse> responseObserver;
+    private final ServerCallStreamObserver<ScoreResponse> responseObserver;
 
     public LiveScoreRequestObserver(StreamObserver<ScoreResponse> responseObserver) {
-        this.responseObserver = responseObserver;
+        this.responseObserver = (ServerCallStreamObserver<ScoreResponse>) responseObserver;
     }
 
     public void handleRequest(ScoreRequest request) {
-        try {
-            // repeat 100 times
-            for (int i = 0; i < 100; i++) {
+        AtomicBoolean canSend = new AtomicBoolean(false);
+        responseObserver.setOnReadyHandler(new Runnable() {
+            @Override
+            public void run() {
+                if (responseObserver.isReady()) {
+                    canSend.set(true);
+                    sendScores(request, responseObserver, canSend);
+                }
+            }
+        });
+    }
+
+    private void sendScores(ScoreRequest request, ServerCallStreamObserver<ScoreResponse> responseObserver, AtomicBoolean canSend) {
+        // repeat 100 times
+        for (int i = 0; i < 100; i++) {
+            if (canSend.get() && responseObserver.isReady()) {
                 logInfo("Sending score: " + i);
                 responseObserver.onNext(
                         ScoreResponse.newBuilder()
@@ -27,12 +43,17 @@ public class LiveScoreRequestObserver {
                                 .setMatchId(request.getMatchId())
                                 .build()
                 );
-                Thread.sleep(200);
+                try {
+                    Thread.sleep(200);
+                } catch (InterruptedException e) {
+                    break;
+                }
+            } else {
+                logInfo("Client not ready!");
+                canSend.set(false);
+                break;
             }
-            responseObserver.onCompleted();
-        } catch (Exception e) {
-            logInfo("LiveScoreRequestObserver: " + e);
-            responseObserver.onError(e);
         }
+        responseObserver.onCompleted();
     }
 }
